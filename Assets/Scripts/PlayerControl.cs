cb6a940a00d0f98d991385c7136aa735df11def2
Jan-Henrik Bruhn <jh.bruhn@me.com>
2015-12-12 15:41:06 +0100

    Implemented Jump Growing

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,11 +4,14 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour {
 
     Rigidbody2D mRigidBody;
+    Transform mTransform;
     public float jumpFactor = 200;
+    public float growingFactor = 1.01f;
 
 	// Use this for initialization
 	void Start () {
         this.mRigidBody = GetComponent<Rigidbody2D>();
+        this.mTransform = GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
@@ -16,6 +19,7 @@ public class PlayerControl : MonoBehaviour {
         if (Input.GetButtonDown("Jump"))
         {
             mRigidBody.AddForce(Vector2.up * jumpFactor);
+            mTransform.localScale = new Vector3(mTransform.localScale.x * growingFactor, mTransform.localScale.x * growingFactor, 1);
         }
 	}
 }
