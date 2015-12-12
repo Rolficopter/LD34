e1d644df24d717c803c06c065de83261d56debaa
Jan-Henrik Bruhn <jh.bruhn@me.com>
2015-12-12 15:24:30 +0100

    Implemented Player Animation

---
+++ Assets/PlayerAnimation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimation : MonoBehaviour {
+
+    public Sprite[] animationSprites;
+
+    int currentSpriteIndex = 0;
+    SpriteRenderer mSpriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+        mSpriteRenderer = GetComponent<SpriteRenderer>();
+
+        InvokeRepeating("Animate", 0, .075f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+
+    void Animate()
+    {
+        mSpriteRenderer.sprite = this.animationSprites[currentSpriteIndex];
+
+        currentSpriteIndex = (currentSpriteIndex + 1) % animationSprites.Length;
+    }
+}
