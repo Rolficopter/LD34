16853e76adb9651b8d2706ccdb6d7c9fd2519941
Jan-Henrik Bruhn <jh.bruhn@me.com>
2015-12-13 00:19:16 +0100

    Implemented Halo for Powerups

--- Assets/Scripts/PowerUpLogic.cs
+++ Assets/Scripts/PowerUpLogic.cs
@@ -1,23 +1,33 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PowerUpLogic : MonoBehaviour {
 
     public bool isExhausted = false;
     public Sprite exhaustedSprite = null;
 
+    Light mHalo;
+    float mInitialRange;
+
 	// Use this for initialization
 	void Start () {
-        
+        mHalo = GetComponent<Light>();
+        mInitialRange = mHalo.range;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+        if (!this.isExhausted)
+        {
+            this.mHalo.range = mInitialRange * (float) Math.Abs(Math.Sin(Time.realtimeSinceStartup * 2));
+        }
 	}
 
     public void Exhaust()
     {
         this.isExhausted = true;
+
+        this.mHalo.enabled = false;
 
         SpriteRenderer spriteRenderer = this.GetComponent<SpriteRenderer>();
         spriteRenderer.sprite = this.exhaustedSprite;
