8220a8aa9d4924867998717724109a419fb0bbf5
Sören Gade <gade.soeren@googlemail.com>
2015-12-12 15:22:00 +0100

    Added basic Main Menu.

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Rolficopter.LD34.Assets.Scripts
+{
+    abstract class Constants
+    {
+        public enum Levels
+        {
+            Menu,
+            Level1
+        }
+
+        public static String GetLevelName(Levels level)
+        {
+            return String.Format("{0}", level.ToString());
+        }
+    }
+}
---
+++ Assets/Scripts/MenuLogic.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Rolficopter.LD34.Assets.Scripts
+{
+    public class MenuLogic : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetButtonDown("Submit"))
+            {
+                this.StartGame();
+            }
+        }
+
+        private void StartGame()
+        {
+            Debug.Log("Loading Level 1...");
+            Application.LoadLevel(Constants.GetLevelName(Constants.Levels.Level1));
+        }
+    }
+}
