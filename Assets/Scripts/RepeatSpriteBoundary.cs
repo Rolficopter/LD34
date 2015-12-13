f30a8243a0b9dade5f3e5947796c0926fa628e67
Jan-Henrik Bruhn <jh.bruhn@me.com>
2015-12-13 21:47:05 +0100

    Implemented Background

--- Assets/Scripts/RepeatSpriteBoundary.cs
+++ Assets/Scripts/RepeatSpriteBoundary.cs
@@ -1,41 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-// @NOTE the attached sprite's position should be "top left" or the children will not align properly
-// Strech out the image as you need in the sprite render, the following script will auto-correct it
-[RequireComponent(typeof(SpriteRenderer))]
 
 // Generates a nice set of repeated sprites inside a streched sprite renderer
 // @NOTE Horizontal only, you can easily expand this to v with a little tweaking
-public class RepeatSpriteBoundary : MonoBehaviour
+namespace Rolficopter.LD34.Assets.Scripts
 {
-    SpriteRenderer sprite;
-
-    void Awake()
+    // @NOTE the attached sprite's position should be "top left" or the children will not align properly
+    // Strech out the image as you need in the sprite render, the following script will auto-correct it
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class RepeatSpriteBoundary : MonoBehaviour
     {
-        // Get the current sprite with an unscaled size
-        sprite = GetComponent<SpriteRenderer>();
-        Vector2 spriteSize = new Vector2(sprite.bounds.size.x / transform.localScale.x, sprite.bounds.size.y / transform.localScale.y);
+        SpriteRenderer sprite;
+    
+        public Sprite secondarySprite;
 
-        // Generate a child prefab of the sprite renderer
-        GameObject childPrefab = new GameObject();
-        SpriteRenderer childSprite = childPrefab.AddComponent<SpriteRenderer>();
-        childPrefab.transform.position = transform.position;
-        childSprite.sprite = sprite.sprite;
-
-        // Loop through and spit out repeated tiles
-        GameObject child;
-        for (int i = 1, l = (int)Mathf.Round(sprite.bounds.size.x); i < l; i++)
+        void Awake()
         {
-            child = Instantiate(childPrefab) as GameObject;
-            child.transform.position = transform.position + (new Vector3(spriteSize.x, 0, 0) * i);
-            child.transform.parent = transform;
+            // Get the current sprite with an unscaled size
+            sprite = GetComponent<SpriteRenderer>();
+            Vector2 spriteSize = new Vector2(sprite.bounds.size.x / transform.localScale.x, sprite.bounds.size.y / transform.localScale.y);
+
+            // Generate a child prefab of the sprite renderer
+            GameObject childPrefab = new GameObject();
+            SpriteRenderer childSprite = childPrefab.AddComponent<SpriteRenderer>();
+            childPrefab.transform.position = transform.position;
+            if (secondarySprite != null && Random.Range(0, 2) == 1)
+            {
+                childSprite.sprite = secondarySprite;
+            } 
+            else
+            {
+                childSprite.sprite = sprite.sprite;
+            }
+            childSprite.sortingOrder = sprite.sortingOrder;
+
+            // Loop through and spit out repeated tiles
+            GameObject child;
+            for (int i = 1, l = (int)Mathf.Round(sprite.bounds.size.x); i < l; i++)
+            {
+                child = Instantiate(childPrefab) as GameObject;
+                child.transform.position = transform.position + (new Vector3(spriteSize.x, 0, 0) * i);
+                child.transform.parent = transform;
+                child.transform.localScale = new Vector3(child.transform.localScale.x, 1, 1);
+            }
+
+            // Set the parent last on the prefab to prevent transform displacement
+            childPrefab.transform.parent = transform;
+            childPrefab.transform.localScale = new Vector3(childPrefab.transform.localScale.x, 1, 1);
+
+            // Disable the currently existing sprite component since its now a repeated image
+            sprite.enabled = false;
         }
-
-        // Set the parent last on the prefab to prevent transform displacement
-        childPrefab.transform.parent = transform;
-
-        // Disable the currently existing sprite component since its now a repeated image
-        sprite.enabled = false;
     }
-}
+    }
