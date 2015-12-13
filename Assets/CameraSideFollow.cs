e43344ce21eed5141a364c57fbe7f3a380ba4fbe
Jan-Henrik Bruhn <jh.bruhn@me.com>
2015-12-13 11:36:41 +0100

    Implemented Camera Follow Script

---
+++ Assets/CameraSideFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSideFollow : MonoBehaviour {
+
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = new Vector3(
+            Mathf.Lerp(transform.position.x, target.transform.position.x, 1f * Time.deltaTime),
+            transform.position.y,
+            transform.position.z);
+	}
+}
